6725ee36c7180ec4eba4ec68beee009adb93dbe3
rowlandwilliams <rowland.williams.94@gmail.com>
2023-02-18 15:29:28 +0000

    add seed script with year of data for each habit

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { PrismaClient } from "@prisma/client";
+import { sqltag } from "@prisma/client/runtime";
+
 const prisma = new PrismaClient();
 async function main() {
-  const surfspot = await prisma.surfSpot.create({
-    data: { name: "Watersplash", latitude: 49.210967, longitude: -2.224461 },
+  await prisma.$executeRaw(
+    sqltag`TRUNCATE TABLE "HabitData" RESTART IDENTITY CASCADE;`
+  );
+  await prisma.$executeRaw(
+    sqltag`TRUNCATE TABLE "Habit" RESTART IDENTITY CASCADE;`
+  );
+
+  await prisma.habit.createMany({
+    data: [
+      { name: "Meditate" },
+      { name: "Go for a run" },
+      { name: "Drawing" },
+      { name: "Connect" },
+      { name: "Make bed" },
+      { name: "Plan life" },
+      { name: "Code" },
+      { name: "Eat Healthy" },
+    ],
   });
-  console.log(surfspot);
+
+  const habits = await prisma.habit.findMany();
+
+  const yearOfDates = [...Array(365)].map((day, i) => {
+    const today = new Date();
+    today.setDate(today.getDate() - 1 * i);
+
+    return today;
+  });
+
+  const sampleData = habits
+    .map(({ id }) =>
+      yearOfDates.map((date) => ({
+        date,
+        completed: Math.random() < 0.5,
+        habitId: id,
+      }))
+    )
+    .flat();
+
+  await prisma.habitData.createMany({
+    data: sampleData,
+  });
 }
 main()
   .then(async () => {
