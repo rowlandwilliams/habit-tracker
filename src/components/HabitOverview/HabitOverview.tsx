3bd650080ee02172a8a5d586f3a38af67020a7e4
rowlandwilliams <rowland.williams.94@gmail.com>
2023-02-25 21:09:06 +0000

    break buttons to components

--- src/components/HabitOverview/HabitOverview.tsx
+++ src/components/HabitOverview/HabitOverview.tsx
@@ -1,14 +1,7 @@
-import classNames from "classnames";
 import { useState } from "react";
 import { useResponsiveGraphWidth } from "../../../hooks/useResponsiveGraphWidth";
 import { HabitOverviewLinkChart } from "./HabitOverviewLinkChart/HabitOverviewLinkChart";
-
-const timeOptions = [
-  { label: "1W", nDays: 7 },
-  { label: "2W", nDays: 14 },
-  { label: "1M", nDays: 30 },
-  { label: "2M", nDays: 60 },
-];
+import { HabitOverviewTimePeriodSelectors } from "./HabitOverviewTimePeriodSelectors/HabitOverviewTimePeriodSelectors";
 
 export const HabitOverview = () => {
   const { ref, graphWidth } = useResponsiveGraphWidth();
@@ -19,24 +12,10 @@ export const HabitOverview = () => {
     <article ref={ref}>
       <section className="flex items-center justify-between">
         <h1>Monthly Breakdown</h1>
-        <div className="space-x-1">
-          {timeOptions.map(({ label, nDays }) => (
-            <button
-              onClick={() => handleTimeOptionClick(nDays)}
-              key={label}
-              className={classNames(
-                "rounded-sm bg-opacity-20 p-1 text-xs font-medium",
-                {
-                  "bg-indigo-500 text-indigo-500": nDays === activeNDays,
-                  "bg-gray-600 text-gray-600 hover:text-purple":
-                    nDays !== activeNDays,
-                }
-              )}
-            >
-              {label}
-            </button>
-          ))}
-        </div>
+        <HabitOverviewTimePeriodSelectors
+          activeNDays={activeNDays}
+          handleTimeOptionClick={handleTimeOptionClick}
+        />
       </section>
       <HabitOverviewLinkChart graphWidth={graphWidth} nDays={activeNDays} />
     </article>
